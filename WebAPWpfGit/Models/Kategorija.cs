8b00585c85b6fe3cce21c580d45ea7555e5b36cc
srdjan.zoranovic@outlook.com <srdjan.zoranovic@outlook.com>
2021-01-19 11:15:55 +0100

    Kreiranje model klasa

---
+++ WebAPWpfGit/Models/Kategorija.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPWpfGit.Models
+{
+    public class Kategorija
+    {
+        [Required]
+        public int KategorijaId { get; set; }
+        [Required(ErrorMessage = "Naziv kategorije je obavezan!")]
+        [StringLength(30, ErrorMessage = "Broj karaktera moze biti izmedjuj 5 i 30!", MinimumLength = 5)]
+        public string NazivKategorije { get; set; }
+        [Required(ErrorMessage = "Opis kategorije je obavezan!")]
+        [StringLength(100, ErrorMessage = "Maksimalan broj karaktera je 100!")]
+        public string OpisKategorije { get; set; }
+    }
+}
---
+++ WebAPWpfGit/Models/Proizvod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPWpfGit.Models
+{
+    public class Proizvod
+    {
+        [Required]
+        public int ProizvodId { get; set; }
+        [Required]
+        [ForeignKey("Kategorija")]
+        public virtual int KategorijaId { get; set; }
+        [Required(ErrorMessage ="Morate unijeti naziv proizvoda!")]
+        [StringLength(30,ErrorMessage ="Naziv proizvoda mora imati izmedju 3 i 30 karaktera!",MinimumLength =3)]
+        public string NazivProizvoda { get; set; }
+        [Required(ErrorMessage ="Morate unijeti cijenu proizvoda!")]
+        [DisplayFormat(DataFormatString ="{0:c}")]
+        public decimal CijenaProizvoda { get; set; }
+        [StringLength(100,ErrorMessage ="Broj karaktera moze biti najvise 100")]
+        public int KolicinaNaLageru { get; set; }
+
+        public virtual Kategorija Kategorija { get; set; }
+    }
+}
